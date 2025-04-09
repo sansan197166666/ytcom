60909fd4471e6430b828ead9016af1f783a2a607
sansan197166666 <sansan197166666@gmail.com>
2025-04-09 10:57:02 +0800

    config.rs

--- src/config.rs
+++ src/config.rs
@@ -98,8 +98,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["tjy.hanridn.com"];//rs-ny.rustdesk.com
-pub const RS_PUB_KEY: &str = "w1EKSdl2JPvAzsfqKaXGtVPKHGMK7v0Va31b4tFD2MQ=";// "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["156.251.19.38"];//rs-ny.rustdesk.com
+pub const RS_PUB_KEY: &str = "63lTBp36DwxgCYFGL5ygOkKK56kPh0KXwlOWNgaqBUE=";// "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
